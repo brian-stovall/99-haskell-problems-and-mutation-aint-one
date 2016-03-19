d1cd03e73c490376f8e48f3d5a22233c0bb58f77
Brian Stovall <hoppenmaus@gmail.com>
2016-03-19 15:34:00 -0400

    ex.3 complete

--- h-99.hs
+++ h-99.hs
@@ -7,6 +7,13 @@ myLast (x:xs)
 myButLast :: [a] -> a
 myButLast [] = error "No next-to-last element in empty list!"
 myButLast [x] = error "No next-to-last element in list of one element!"
-myButLast [x:xs]
+myButLast (x:xs)
   | length xs == 1 = x
   | otherwise = myButLast xs
+
+elementAt' :: Int -> [a] -> a
+elementAt' _ [] = error "List shorter than requested element!"
+elementAt' n (x:xs)
+  | n < 1 = error "Element requested must be >= 1!"
+  | n == 1    = x
+  | otherwise = elementAt' (n-1) xs
