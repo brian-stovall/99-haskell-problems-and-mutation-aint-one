8c35503c2503f72033d906da84e925fe84e6e224
Brian Stovall <hoppenmaus@gmail.com>
2016-03-19 20:40:36 -0400

    seven down

--- h-99.hs
+++ h-99.hs
@@ -1,9 +1,11 @@
+-- 1
 myLast :: (Eq a) => [a] -> a
 myLast [] = error "No last element for an empty list!"
 myLast (x:xs)
   | xs == []  = x
   | otherwise = myLast(xs)
 
+--2
 myButLast :: [a] -> a
 myButLast [] = error "No next-to-last element in empty list!"
 myButLast [x] = error "No next-to-last element in list of one element!"
@@ -11,9 +13,32 @@ myButLast (x:xs)
   | length xs == 1 = x
   | otherwise = myButLast xs
 
+--3
 elementAt' :: Int -> [a] -> a
 elementAt' _ [] = error "List shorter than requested element!"
 elementAt' n (x:xs)
   | n < 1 = error "Element requested must be >= 1!"
   | n == 1    = x
   | otherwise = elementAt' (n-1) xs
+
+--4
+myLength :: [a] -> Int
+myLength as = foldl (\acc _-> acc + 1) 0 as 
+
+--5
+myReverse :: [a] -> [a]
+myReverse [] = []
+myReverse (x:xs) = myReverse xs ++ [x]
+
+--6
+isPalindrome :: (Eq a) => [a] -> Bool
+isPalindrome as
+  | as == myReverse as = True
+  | otherwise          = False
+
+--7 TODO
+
+--8
+compress :: (Eq a) => [a] -> [a]
+compress (a:[]) = [a]
+compress (a:as) = if (a == head as) then compress as else a : (compress as)
